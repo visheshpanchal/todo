6374687df146041c2ca7a29c3fb6fe8567dd5b7e
Vishesh Panchal <visheshpanchal145@gmail.com>
2022-01-28 15:41:50 +0530

    secret key hide

--- todo/settings.py
+++ todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y2i-my%pqyh^^dz-d89um8+2v$_*s#%xq&d2^l7yg50s^3=#v$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
